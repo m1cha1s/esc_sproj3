bb2021f1ec025855ea8901ee368cf5a406f2cb51
m1cha1s <msulej04@gmail.com>
2024-10-08 15:06:18 +0200

    Initialized the timer and started work on generating the trapezoid waveform

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#include "hardware/pwm.h"
+
 int pico_led_init() {
     gpio_init(PICO_DEFAULT_LED_PIN);
     gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
@@ -11,7 +13,24 @@ void pico_set_led(bool val) {
     gpio_put(PICO_DEFAULT_LED_PIN, val);
 }
 
+void init_pwm(void) {
+    gpio_set_function(
+}
+
+
+bool callback(struct repeating_timer *t) {
+    bool *val = (bool*)t->user_data;
+    pico_set_led(*val);
+    *val = !(*val);
+
+    return true;
+}
+
 int main() {
+    bool val = false;
+    struct repeating_timer timer = {0};
+    add_repeating_timer_ms(-10, callback, &val, &timer);
+    
     stdio_init_all();
 
     int rc = pico_led_init();
@@ -19,11 +38,8 @@ int main() {
 
     long i = 0;
 
-    bool val = false;
     while (true) {
-        pico_set_led(val);
-        val = !val;
-        sleep_ms(100);
+        sleep_ms(10);
         printf("Hello world!\t%d\n", i);
         i++;
     }
