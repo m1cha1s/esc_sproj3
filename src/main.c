cc80e0dc968a66deb2817dad1352621d8718a1fd
m1cha1s <msulej04@gmail.com>
2024-11-29 13:02:43 +0100

    Still no workie

--- src/main.c
+++ src/main.c
@@ -7,14 +7,22 @@
 #include "pico/stdlib.h"
 #include "pico/multicore.h"
 #include "hardware/pwm.h"
-#include "hardware/adc.h"
-#include "hardware/dma.h"
+#include "hardware/gpio.h"
+#include "hardware/timer.h"
+
+
+typedef enum
+{
+    MODE_OPEN_LOOP,
+    MODE_CLOSED_LOOP,
+} Mode;
+
 
 void pico_led_init(void);
 void pico_set_led(bool val);
 
-void InitPins(void);
-void InitADC(void);
+void InitPins( void );
+void InitADC( void );
 
 #define P0_PIN 10
 #define P1_PIN 11
@@ -24,130 +32,104 @@ void InitADC(void);
 #define PN1_PIN 14
 #define PN2_PIN 15
 
+#define SENSE_PIN 16
+#define DEBOUNCE 100
+
+#define PWM_MAX 15
+
 const int pins[] = {P0_PIN, P1_PIN, P2_PIN, PN0_PIN, PN1_PIN, PN2_PIN};
 
-#define ADC_PIN 26
-
-#define PWM_PERIOD_PO2 9
-
-#define PWM_PIN PICO_DEFAULT_LED_PIN
-
-#define Lerp(a, b, t) (a+t*(b-a))
-
 repeating_timer_t timer = {0};
-
-/* 0 1 1 0 0 0 */
 
 bool p[6] = {1, 1, 0, 0, 0, 0};
 bool n[6] = {0, 0, 0, 1, 1, 0};
 
 int pos = 0;
 
-volatile bool trig = false;
+volatile u64 lastSenseTime = 0;
+volatile u64 stepDuration = 1000;
+volatile s32 step = 0;
+volatile  u8 driveLevel = 0;
+volatile f32 freq = 0;
 
-void core1();
+volatile bool spinup = true;
 
-int main()
+void logging();
+void spinUp();
+
+void main()
 {
-    stdio_init_all();
+    multicore_launch_core1(logging);
+
     pico_led_init();
     InitPins();
-    InitADC();
 
-    multicore_launch_core1(core1);
-    while(1)
-    {
-        // printf("%d: %d\n", multicore_fifo_pop_blocking(), multicore_fifo_pop_blocking());
-        printf("%d\n", multicore_fifo_pop_blocking());
-        // u32 val = multicore_fifo_pop_blocking();
-        // u32 n = (val>>16);
-        // u32 counts = (val & 0xffff);
-        // printf("%d: %d\n", n, counts);
-    }
-}
-
-void usb_log(s32 val) 
-{
-    if (multicore_fifo_wready())
-    {
-        /* multicore_fifo_push_blocking(spinningness); */
-        multicore_fifo_push_blocking(val);
-    }
-}
-
-void core1()
-{
     bool led = false;
-    f32 time = 0;
-    f32 step = 0.001;
-    s32 next_check = 0;
-    s32 adc_values[6] = {0};
+    // u64 nextStep = 0;
 
-    f32 ramp = 0;
+    pico_set_led(false);
+
+    spinUp();
+    driveLevel = PWM_MAX/2;
+
+    pico_set_led(true);
 
     while (true)
     {
-        if (time > (f32)next_check+0.3f)
+        if ((lastSenseTime + stepDuration*(step+1)) < time_us_64())
         {
-            adc_values[next_check] = adc_read();
-            // adc_values[next_check] /= 2;
-            next_check += 1;
+            step++;
+            step %= 6;
 
-            // u32 val = 0;
-            // val |= ((next_check-1)<<16);
-            // val |= (0xffff & adc_values[next_check-1]);
-            // usb_log(val);
+            gpio_put(P0_PIN, p[(step)%6]);
+            gpio_put(P1_PIN, p[(step+2)%6]);
+            gpio_put(P2_PIN, p[(step+4)%6]);
 
-            // usb_log(next_check-1);
-            // usb_log(adc_values[next_check-1]);
-            
-            if (next_check >= 6)
+            pwm_set_gpio_level(PN0_PIN, n[(step)%6]   ? driveLevel : 0);
+            pwm_set_gpio_level(PN1_PIN, n[(step+2)%6] ? driveLevel : 0);
+            pwm_set_gpio_level(PN2_PIN, n[(step+4)%6] ? driveLevel : 0);
+            // printf("%f\n", freq);
+        }
+    }
+}
+
+void spinUp()
+{
+    stepDuration = 4000;
+    spinup = true;
+    driveLevel = PWM_MAX;
+
+    while(stepDuration > 1000 && spinup)
+    {
+        if ((lastSenseTime + stepDuration*(step+1)) < time_us_64())
+        {
+            step++;
+            stepDuration -= 5;
+            if (step >= 6)
             {
-                next_check = 0;
-                
-                s32 avrg = 0;
-                for (s32 i = 0; i < Len(adc_values); ++i)
-                {
-                    avrg += adc_values[i]/Len(adc_values);
-                }
-                s32 maxDelta = 0;
-                for (s32 i = 0; i < Len(adc_values); ++i)
-                {
-                    maxDelta = Max(Abs(avrg-adc_values[i]), maxDelta);
-                }
-
-                usb_log(avrg);
-                
-                // s32 spinningness = adc_values[1]-adc_values[4];
-
-                // if (spinningness < 0)
-                // {
-                //     usb_log(adc_values[1]);
-                // }
-               
-                // usb_log(spinningness);
-            
-                // if (spinningness < 50 && ramp >= 0.15)
-                // {
-                //      ramp = 0;
-                // }
+                step = 0;
+                lastSenseTime = time_us_64();
             }
-        }
+            gpio_put(P0_PIN, p[(step)%6]);
+            gpio_put(P1_PIN, p[(step+2)%6]);
+            gpio_put(P2_PIN, p[(step+4)%6]);
 
-        gpio_put(P0_PIN, p[((int)time)%6]);
-        gpio_put(P1_PIN, p[((int)time+2)%6]);
-        gpio_put(P2_PIN, p[((int)time+4)%6]);
-
-        gpio_put(PN0_PIN, n[((int)time)%6]);
-        gpio_put(PN1_PIN, n[((int)time+2)%6]);
-        gpio_put(PN2_PIN, n[((int)time+4)%6]);
-        
-        time += Lerp(step, 0.01, ramp);
-        if (time > 6.f)
-        {
-            time = 0;
-            if (ramp < 1) ramp += 0.002;
+            pwm_set_gpio_level(PN0_PIN, n[(step)%6]   ? driveLevel : 0);
+            pwm_set_gpio_level(PN1_PIN, n[(step+2)%6] ? driveLevel : 0);
+            pwm_set_gpio_level(PN2_PIN, n[(step+4)%6] ? driveLevel : 0);
         }
+    }
+
+    spinup = false;
+}
+
+void logging()
+{
+    stdio_init_all();
+
+    while (true)
+    {
+        printf("%llu, %llu\n", lastSenseTime, stepDuration);
     }
 }
 
@@ -162,23 +144,49 @@ void pico_set_led(bool val)
     gpio_put(PICO_DEFAULT_LED_PIN, val);
 }
 
+void senseCb(uint gpio, u32 events)
+{
+    static int pulseCounter = 10;
+
+
+    u64 now = time_us_64();
+
+    for (s32 i = 0; i < DEBOUNCE; ++i)
+    {
+        if (!gpio_get(SENSE_PIN)) return;
+    }
+
+    u64 spinDuration = now - lastSenseTime;
+
+    // printf("\t%llu\n", spinDuration);
+    // if (spinDuration <= stepDuration*2) return;
+
+    freq = (f32)1e6 / (f32)spinDuration;
+    stepDuration = spinDuration / 6;
+    lastSenseTime = now;
+    //step = 0;
+    // spinup = false;
+}
+
 void InitPins(void)
 {
-
-    for (int i = 0; i < Len(pins); ++i)
+    for (int i = 0; i < 3; ++i)
     {
         gpio_init(pins[i]);
         gpio_set_dir(pins[i], GPIO_OUT);
     }
+
+    for (int i = 3; i < 6; ++i)
+    {
+        gpio_set_function(pins[i], GPIO_FUNC_PWM);
+        u32 sliceNum = pwm_gpio_to_slice_num(pins[i]);
+        // pwm_set_clkdiv(sliceNum, 50);
+        pwm_set_wrap(sliceNum, PWM_MAX);
+        pwm_set_gpio_level(pins[i], 0);
+        pwm_set_enabled(sliceNum, true);
+    }
+
+    gpio_init(SENSE_PIN);
+    gpio_set_dir(SENSE_PIN, GPIO_IN);
+    gpio_set_irq_enabled_with_callback(SENSE_PIN, GPIO_IRQ_EDGE_RISE, true, &senseCb);
 }
-
-void InitADC(void)
-{
-    adc_init();
-
-    adc_gpio_init(ADC_PIN);
-    adc_select_input(0);
-
-    adc_set_clkdiv(0);
-}
-
